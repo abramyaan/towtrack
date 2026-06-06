3f76e9c204deb2ab88cb1893f96ba575b6c4c5e3
abramyaan <abramyan_ero@mail.ru>
2026-06-06 19:31:24 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/tow-track/",
+  base: "/towtrack/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
